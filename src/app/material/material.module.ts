efb3f1bfd57d739f3c63e3b1d8587c6d8201e178
mark <markpackham1@gmail.com>
2022-03-15 14:39:37 +0000

    demo badge position
    https://youtu.be/SiIPlZAOnQ4?t=182

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
-const MaterialComponents = [MatButtonModule, MatButtonToggleModule, MatIconModule]
+const MaterialComponents = [MatButtonModule, MatButtonToggleModule, MatIconModule, MatBadgeModule]
 
 @NgModule({
   imports: [
