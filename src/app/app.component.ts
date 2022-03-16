1ba0c7ab6595fe20009a279eb43a949d945e0482
mark <markpackham1@gmail.com>
2022-03-16 13:02:30 +0000

    demo progress spinner with set timeout
    https://www.youtube.com/watch?v=3gtczOujFV0&list=PLC3y8-rFHvwilEuCqFGTL5Gt5U6deIrsU&index=10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular_material_learn';
   notifications = 0;
+  showSpinner = false;
+
+  loadData(){
+    this.showSpinner = true;
+    setTimeout(() =>{
+this.showSpinner = false
+    },5000)
+  }
 }
