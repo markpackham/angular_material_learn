8957ebc8447bcc04242598365a030246a69b04cb
mark <markpackham1@gmail.com>
2022-03-21 11:49:38 +0000

    finish autocomplete to only show relevant options
    https://www.youtube.com/watch?v=G73PWSFDlmw&list=PLC3y8-rFHvwilEuCqFGTL5Gt5U6deIrsU&index=22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   options: string[] = ['Angular','React','Vue'];
   objectOptions = [
@@ -14,6 +17,21 @@ export class AppComponent {
     {name: 'React'},
     {name: 'Vue'}
   ];
+
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]> | undefined;
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(startWith(''), map(value => this._filter(value)));
+  }
+
+  private _filter(value: string): string[]{
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+
+
 
   displayFunction(subject: { name: any; }){
     return subject ? subject.name : undefined
