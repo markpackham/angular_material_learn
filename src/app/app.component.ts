c86b666beec185fbd7feb0177a3bbf8e3e3c28e0
mark <markpackham1@gmail.com>
2022-03-22 14:14:49 +0000

    set min and max dates
    https://youtu.be/l7iDFzW0jl0?t=319

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  options: string[] = ['Angular','React','Vue'];
-  objectOptions = [
-    {name: 'Angular'},
-    {name: 'Angular Material'},
-    {name: 'React'},
-    {name: 'Vue'}
-  ];
-
-  myControl = new FormControl();
-  filteredOptions: Observable<string[]> | undefined;
-
-  ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(startWith(''), map(value => this._filter(value)));
-  }
-
-  private _filter(value: string): string[]{
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-
-
-
-  displayFunction(subject: { name: any; }){
-    return subject ? subject.name : undefined
-  }
+export class AppComponent {
+  minDate = new Date();
+  maxDate = new Date(2025, 3, 10);
 }
