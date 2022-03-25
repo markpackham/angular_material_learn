dcd19e52465be93fce517da2b96e2588a061cca7
mark <markpackham1@gmail.com>
2022-03-25 14:46:20 +0000

    attempting to filter based on event
    https://youtu.be/ZhcYPXLGr_E?t=193

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,9 @@ export class AppComponent {
     console.log(row);
   }
 
+  // filterValue: string
+
   applyFilter(filterValue: string){
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.filter = filterValue.trim();
   }
 }
