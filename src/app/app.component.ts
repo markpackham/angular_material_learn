ae0c0a59c5ec982446346ae11f7e5b74f98f55d1
mark <markpackham1@gmail.com>
2022-03-25 14:23:31 +0000

    adding filtering for data table
    https://youtu.be/ZhcYPXLGr_E?t=161

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
@@ -27,10 +28,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AppComponent {
   displayedColumns: string[] = ['position', 'name', 'symbol', 'weight'];
-  displayedColumnsData: string[] = ['position', 'name', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   logData(row: any){
     console.log(row);
+  }
+
+  applyFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
